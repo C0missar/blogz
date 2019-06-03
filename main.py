72604adb7b6f34893519bb5e52dc7f2eb8c8f503
RiverSelkie <beth@wardsonline.net>
2019-06-03 16:08:11 -0500

    added login, logout, posts by user

--- main.py
+++ main.py
@@ -1,72 +1,116 @@
-from flask import Flask, request, redirect, render_template
+from flask import Flask, request, redirect, render_template, session, flash
 from flask_sqlalchemy import SQLAlchemy 
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:hello@localhost:8889/build-a-blog'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://blogz:blogz@localhost:8889/blogz'
 app.config['SQLALCHEMY_ECHO'] = True 
 db = SQLAlchemy(app)
+
+class User (db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(120))
+    password = db.Column(db.String(120))
+    blogs = db.relationship("Entry", backref = "user")
+
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
 
 class Entry(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(120))
     body = db.Column(db.String(2000))
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
 
-    def __init__(self, title, body):
+
+    def __init__(self, title, body, user):
         self.title = title
         self.body = body
+        self.user = user
+
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    incorrect_info=""
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        incorrect_info=""
+        q_user = User.query.filter_by(username=username).first()
+        error_bool=False
+        if q_user:
+            if password != q_user.password:
+                incorrect_info = "Incorrect username or password"
+                error_bool=True
+        else:
+            incorrect_info = "Incorrect username or password"
+            error_bool=True    
+        if error_bool == False:
+            session['user'] = username 
+            return redirect('/newpost?username='+username)
+        else:
+            return render_template("login.html", incorrect_info=incorrect_info)    
+    return render_template("login.html")
+    
+@app.route('/logout')
+def logout():
+    if "user" in session:
+        del session['user']
+    return redirect('/blog')
+
 
 @app.route("/signup", methods=['GET', 'POST'])
 def signup():
     mismatch=""
     bad_password=""
     bad_username=""
-    bad_email=""
+    other_username=""
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
         verify = request.form['verify']
-        email = request.form['email']
-        mismatch=""
-        bad_password=""
-        bad_username=""
+        taken_username=User.query.filter_by(username=username).count()
         error_bool=False
         if verify != password:
             mismatch="These passwords do not match."
             error_bool=True
-        if len(password) <3 or len(password) >20:
-            bad_password="Please enter a password that is between 3 and 20 characters long"
+        if len(password) <3:
+            bad_password="Please enter a password that is at least 3 characters long"
             error_bool=True
-        if len(username) <3 or len(username) >20:
-            bad_username="Please enter a username that is between 3 and 20 characters long"    
+        if len(username) <3:
+            bad_username="Please enter a username that is at least 3 characters long"    
             error_bool=True
-        if is_email(email) == False and len(email) > 0:
-            bad_email="That email didn't turn out to be structured correctly.  Please try again."
+        if taken_username > 0:
+            other_username="This username has already been taken.  Please pick another one."
             error_bool=True
         if error_bool == False:  
-            return redirect('/welcome?username='+username)
-    return render_template("signup.html", mismatch=mismatch, bad_password=bad_password, bad_username=bad_username, bad_email=bad_email)
-
-def is_email(email):
-    find_at = email.find('@')
-    has_at = find_at >= 1
-    if not has_at:
-        return False
-    find_dot = email.find('.')
-    correct_dot = find_dot > find_at
-    if not correct_dot:
-        return False
-    else:
-        return True
+            user = User(username, password)
+            db.session.add(user)
+            db.session.commit()
+            session["user"] = username
+            return redirect('/newpost?username='+username)
+        else:
+            return render_template("signup.html", mismatch=mismatch, bad_password=bad_password, bad_username=bad_username, other_username=other_username)
+    else: 
+        return render_template("signup.html")
 
 
 @app.route('/blog')
 def optimus_prime():
+    #TODO 4: have a second query param for user id, which 
+    # when going down that route will serve up a page
+    # of all the entries written by a single user.
     boomstick=request.args.get("boomstick")
-    if boomstick is None:
+    seahorse=request.args.get("seahorse")
+    if boomstick is None and seahorse is None:
         entries = Entry.query.all()
-        return render_template('blog.html', entries=entries) 
+        return render_template('blog.html', entries=entries)
+    elif seahorse:
+        user_entries = User.query.get(int(seahorse))
+        return render_template('blog.html', entries=user_entries.blogs)
     else:
         entry = Entry.query.get(int(boomstick))
         return render_template('blog.html', entries=[entry])
@@ -80,22 +124,21 @@ def avocado():
     else:    
         entry_title = request.form['title']
         entry_body = request.form['body']
+        error_bool = False
         if len(entry_title) < 1:
             no_title = "Please enter a title"
             error_bool = True
         if len(entry_body) < 1:
             no_body = "Please enter some text"
             error_bool = True
-        if error_bool == False:    
-            new_entry = Entry(entry_title, entry_body)
+        if error_bool == False:
+            owner=User.query.filter_by(username = session['user']).first()    
+            new_entry = Entry(entry_title, entry_body, owner)
             db.session.add(new_entry)
             db.session.commit()
             return redirect('/blog?boomstick=' + str(new_entry.id))
         else: 
             return render_template('/newpost.html', no_title=no_title, no_body=no_body)
-@app.route('/')
-def seahorse():
-    return redirect('/blog')
 
 
 @app.route("/welcome")
@@ -107,5 +150,12 @@ def welcome_in():
 def index():
     return redirect("/signup")
 
+endpoints_with_login = ["avocado"]
+
+@app.before_request
+def require_login():
+    if ("user" not in session and request.endpoint in endpoints_with_login):
+        return redirect("/login")
+app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RU'
 if __name__ == "__main__":
     app.run()
